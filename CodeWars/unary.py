a457ee8aadb02f04d4b2cbe87294f126b4101143
lucifersasi <60611428+lucifersasi@users.noreply.github.com>
2021-05-14 09:27:25 +0530

    git commit -m "defillibrators and Unary"

---
+++ CodeWars/unary.py
@@ -0,0 +1,27 @@
+
+# INPUT =  5
+# 1 11 111 1111 11111
+# OUTPUT =     111111111111111
+
+
+# Sasi
+
+import sys
+import math
+
+
+# count = int(input())
+# k =""
+# for unary in input().split():
+#     k = k + unary
+# print(k)
+
+# guru1
+# input()
+# print(''.join(input().split()))
+
+# guru2
+
+input()
+count = input()
+print(*count.split(), sep="")
